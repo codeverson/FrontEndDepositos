1d330185e227aceb61583d07872c1d7138bc2db7
Everson Araujo <araujoeverson@icloud.com>
2023-04-27 21:43:54 -0300

    Final do módulo 7

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false;
 
 function validaNome(nomeCompleto) {
     let nomeComoArray = nomeCompleto.split(' ');
@@ -6,10 +8,9 @@ function validaNome(nomeCompleto) {
 }
 
 form.addEventListener('submit', function(e) {
-    let formEValido = false;
+
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
     const mensagemSucesso = `Montante de <b>R$${valorDeposito.value}</b> depositado para o cliente <b>${nomeBeneficiario.value}<b/>. Conta: <b>${numeroContaBeneficiario.value}<b/>`;
@@ -37,4 +38,17 @@ form.addEventListener('submit', function(e) {
     }
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEValido = validaNome (e.target.value);
+    
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block';
+
+    } else {
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+
+    }
+})
